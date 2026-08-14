03a3e95a3b4cfddebdff5f6f56ac8a55be36582d
hasib lakdawala <138361234+hasiblkd@users.noreply.github.com>
2026-08-14 14:36:50 +0530

    all

--- Python/assigment/Module 2/Session 6/que4.py
+++ Python/assigment/Module 2/Session 6/que4.py
@@ -1,4 +1,14 @@
 movie_title=['Inception', 'Avatar', 'Joker']
 genres=['Sci-Fi', 'Action', 'Drama']
 rating=[8.8, 7.8, 8.4]
-new_movie=zip()
+
+movie_list=[]
+
+for title,genre,rate in zip(movie_title,genres,rating):
+    movie_list.append({
+        title:"title",
+        genre:"Genres",
+        rate:"Rating"
+    })
+
+print("Movie Name:-",movie_list)
